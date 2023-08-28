fix gradient issue

LatentPaintTrainer.py
@@ -207,7 +207,7 @@ class LatentPaintTrainer:
 
     def init_optimizer(self, params_to_train) -> Optimizer:
         # optimizer = torch.optim.Adam(self.mesh_model.get_params(), lr=self.cfg.optim.lr, betas=(0.9, 0.99), eps=1e-15)
-        optimizer = torch.optim.Adam(params_to_train, lr=self.cfg.optim.lr, betas=(0.9, 0.99), eps=1e-15)
+        optimizer = torch.optim.Adam(params_to_train, lr=self.cfg.optim.lr, betas=(0.9, 0.99), eps=1e-6)
         return optimizer
 
     def init_dataloaders(self) -> Dict[str, DataLoader]:
@@ -234,7 +234,7 @@ class LatentPaintTrainer:
         # self.evaluate(self.dataloaders['val'], self.eval_renders_path)
         # self.mesh_model.train()
 
-        # self.evaluate(self.eval_renders_path)
+        self.evaluate(self.eval_renders_path)
         self.color_network.train()
         if self.latent:
             self.nerf_outside.train()
@@ -247,12 +247,14 @@ class LatentPaintTrainer:
             for i in range(self.img_dataset.n_images):
                 self.train_step += 1
                 pbar.update(1)
-                torch.autograd.set_detect_anomaly(True)
+                # torch.autograd.set_detect_anomaly(True)
                 self.optimizer.zero_grad()
                 # pred: (H, W, color_ch)
                 pred, loss = self.train_render(i) # render ith image
                 nn.utils.clip_grad_norm_(self.color_network.parameters(), 1.0)
                 self.optimizer.step()
+                # print("After step: ")
+                # self.color_network.test()
                 if np.random.uniform(0, 1) < 0.05:
                     # Randomly log rendered images throughout the training                
                     self.log_train_renders(pred, i)
@@ -425,11 +427,12 @@ class LatentPaintTrainer:
         return img_fine, sampled_color
     
     def check_all_grad(self):    
+        grad_error = False
         for name, param in self.color_network.named_parameters():
-            # if param.grad is not None:
-            print(f'Layer {name} grad: ', param.grad)
             if param.grad is not None and torch.isnan(param.grad).any():
                 print(f"Layer {name} has NaN gradients")
+                grad_error = True
+        return grad_error
 
     def check_all_isnan(self):
         has_nan = False
@@ -471,7 +474,12 @@ class LatentPaintTrainer:
         # for debug
         # pred.retain_grad()
         # sampled_color.retain_grad()
-
+        # grad_has_nan = self.check_all_grad()
+        # has_nan = self.check_all_isnan()
+        # if has_nan or grad_has_nan:
+          #   raise NotImplementedError
+        # print("Before test===================")
+        # self.color_network.test()
         loss_guidance = self.diffusion.train_step(text_z, pred, params_to_train=list(self.color_network.parameters()))
         loss = loss_guidance # Note: this loss value will be 0. The real loss value can't be calculated
         loss = -1
@@ -483,12 +491,13 @@ class LatentPaintTrainer:
         # print(sampled_color.grad)
         # print(torch.any(torch.isnan(pred.grad)))
         # print(torch.any(torch.isnan(sampled_color)))
-        self.check_all_grad()
-        has_nan = self.check_all_isnan()
-        if has_nan:
-            raise NotImplementedError
+        # grad_has_nan = self.check_all_grad()
+        # has_nan = self.check_all_isnan()
+        # if has_nan or grad_has_nan:
+          #   raise NotImplementedError
         # raise NotImplementedError
-
+        # print("After test===================")
+        # self.color_network.test()
         pred = pred
         if not self.latent:
             # (1, 3, H, W) -> (H, W, 3)

models/fields.py
@@ -207,19 +207,42 @@ class RenderingNetwork(nn.Module):
             rendering_input = torch.cat([points, view_dirs, feature_vectors], dim=-1)
 
         x = rendering_input
-
+        # print("first x: ", x)
         for l in range(0, self.num_layers - 1):
             lin = getattr(self, "lin" + str(l))
 
             x = lin(x)
-
+            if torch.isnan(x).any():
+                print(f"linear output at layer {l}: ", x)
+                print("parameters: ", lin.weight, lin.bias)
+                raise NotImplementedError
             if l < self.num_layers - 2:
                 x = self.relu(x)
-
+            if torch.isnan(x).any():
+                print(f"relu output at layer {l}: ", x)
+                raise NotImplementedError
+            
         if self.squeeze_out:
             x = torch.sigmoid(x)
+        # print("final x: ", x)
         return x
 
+    def test(self):
+        lin = getattr(self, "lin0")
+        print("weights: ", lin.weight)
+        print("weights grad: ", lin.weight.grad)
+        print("bias: ", lin.bias)
+        print("bias grad", lin.bias.grad)
+        print("weights type: ", type(lin.weight))
+        print("bias type: ", type(lin.bias))
+        print("weights requires_grad: ", lin.weight.requires_grad)
+        print("bias requires_grad: ", lin.bias.requires_grad)
+        if torch.isnan(lin.weight).any() or torch.isnan(lin.bias).any()\
+            or (lin.weight.grad is not None and torch.isnan(lin.weight.grad).any())\
+            or (lin.bias.grad is not None and torch.isnan(lin.bias.grad).any()):
+            print("weight error: ", torch.isnan(lin.weight).any())
+            print("bias error: ", torch.isnan(lin.bias).any())
+            raise NotImplementedError
 
 # This implementation is borrowed from nerf-pytorch: https://github.com/yenchenlin/nerf-pytorch
 # Normal NeRF: output_ch = 4
@@ -292,7 +315,7 @@ class NeRF(nn.Module):
             h = F.relu(h)
             if i in self.skips:
                 h = torch.cat([input_pts, h], -1)
-        print("first h: ", h)
+        
         if self.use_viewdirs:
             alpha = self.alpha_linear(h)
             feature = self.feature_linear(h)
@@ -301,11 +324,9 @@ class NeRF(nn.Module):
             for i, l in enumerate(self.views_linears):
                 h = self.views_linears[i](h)
                 h = F.relu(h)
-                print(f"{i}th h: ", h)
+                
 
             rgb = self.rgb_linear(h)
-            print("rgb: ", rgb)
-            print("alpha: ", alpha)
             return alpha, rgb
         else:
             assert False # enforced to use view direction

models/renderer.py
@@ -736,7 +736,7 @@ class LatentPaintRenderer:
         if torch.isnan(sampled_color.any()):
             print("sampled_color error: ", sampled_color)
             raise NotImplementedError
-        print("sampled color---------",sampled_color)
+        # print("sampled color---------",sampled_color)
         #sampled_color = color_network(pts, gradients, dirs, feature_vector).reshape(batch_size, n_samples, 4) # latent color
 
         inv_s = deviation_network(torch.zeros([1, 3]))[:, :1].clip(1e-6, 1e6)           # Single parameter

stable_diffusion.py
@@ -125,8 +125,8 @@ class StableDiffusion(nn.Module):
             pred_rgb_512 = F.interpolate(inputs, (512, 512), mode='bilinear', align_corners=False)
             latents = self.encode_imgs(pred_rgb_512)
         else:
-            latents = inputs
-            # latents = F.interpolate(inputs, (64, 64), mode='bilinear', align_corners=False)
+            # latents = inputs
+            latents = F.interpolate(inputs, (64, 64), mode='bilinear', align_corners=False)
         # torch.cuda.synchronize(); print(f'[TIME] guiding: interp {time.time() - _t:.4f}s')
         # timestep ~ U(0.02, 0.98) to avoid very high/low noise level
         t = torch.randint(self.min_step, self.max_step + 1, [1], dtype=torch.long, device=self.device)
@@ -162,10 +162,10 @@ class StableDiffusion(nn.Module):
         # _t = time.time()
         # for param in params_to_train:
           #   print(param.grad)
-        print("gradient: ", grad)
-        with torch.autograd.detect_anomaly():
+        # print("gradient: ", grad)
+        # with torch.autograd.detect_anomaly():
             # latents.backward(gradient=grad, retain_graph=True)
-            latents.backward(gradient=grad, retain_graph=True)
+        latents.backward(gradient=grad, retain_graph=True)
         # torch.autograd.grad(outputs=latents, inputs=params_to_train, grad_outputs=grad)
         # for param in params_to_train:
           #   print(param.grad)